40e953942eb7d36306689d914d669b1fd887611d
mobasuite <59516556+mobasuite@users.noreply.github.com>
2021-06-04 16:08:02 +0200

    Delete mobasuite.h

--- mobasuite.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "resource.h"
-// Custom MOBASuite Header Files
-#include <Shlobj_core.h>
-#include <Tlhelp32.h>
-#include <filesystem>
-#include <shellapi.h>
-#include <CommCtrl.h>
